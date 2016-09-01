914c32c1a60c71665948ebe4a2394e21a08e7fb1
Ishai Hachlili <ishai@ishaihachlili.com>
2016-08-31 23:57:47 -0700

    Add project files.

---
+++ MvvmCross.Android.Controls.Sample.Core/Class1.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MvvmCross.Android.Controls.Sample.Core
+{
+    public class Class1
+    {
+    }
+}
